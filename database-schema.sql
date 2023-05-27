6ce61ab3cd8035691ac13bd699c080d03591b004
Nakupenda <116508057+Nakupenda25@users.noreply.github.com>
2023-05-27 17:50:01 +0200

    Updated SQL-File

--- database-schema.sql
+++ database-schema.sql
@@ -1,8 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: uebung07
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `courses` (
+  `course_id` int NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`course_id`,`course_name`),
+  UNIQUE KEY `course_name_UNIQUE` (`course_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `courses`
+--
+
+LOCK TABLES `courses` WRITE;
+/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
+INSERT INTO `courses` VALUES (4,'Algorithms'),(3,'Databases'),(5,'English'),(1,'Maths'),(2,'Programming');
+/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
+UNLOCK TABLES;
+
 --
 -- Table structure for table `event_registrations`
 --
-DROP TABLE IF EXISTS `event_registrations`;
 
+DROP TABLE IF EXISTS `event_registrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `event_registrations` (
   `registration_id` int NOT NULL AUTO_INCREMENT,
   `student_id` int DEFAULT NULL,
@@ -10,13 +54,26 @@ CREATE TABLE `event_registrations` (
   PRIMARY KEY (`registration_id`),
   KEY `student_id` (`student_id`),
   CONSTRAINT `event_registrations_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `users` (`user_id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event_registrations`
+--
+
+LOCK TABLES `event_registrations` WRITE;
+/*!40000 ALTER TABLE `event_registrations` DISABLE KEYS */;
+INSERT INTO `event_registrations` VALUES (2,4,'English'),(3,4,'Databases'),(15,4,'Programming'),(16,4,'Maths'),(19,2,'Maths'),(20,2,'Programming'),(21,2,'English'),(22,2,'Databases');
+/*!40000 ALTER TABLE `event_registrations` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `events`
 --
-DROP TABLE IF EXISTS `events`;
 
+DROP TABLE IF EXISTS `events`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `events` (
   `event_id` int NOT NULL AUTO_INCREMENT,
   `event_room` varchar(255) DEFAULT NULL,
@@ -26,16 +83,55 @@ CREATE TABLE `events` (
   `event_start_time` time DEFAULT NULL,
   `event_end_time` time DEFAULT NULL,
   PRIMARY KEY (`event_id`),
-  KEY `room_id` (`event_room`),
   KEY `admin_id` (`admin_id`),
-  CONSTRAINT `events_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `users` (`user_id`)
-);
+  KEY `room` (`event_room`),
+  KEY `course_key_idx` (`event_name`),
+  CONSTRAINT `course_key` FOREIGN KEY (`event_name`) REFERENCES `courses` (`course_name`),
+  CONSTRAINT `events_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `room` FOREIGN KEY (`event_room`) REFERENCES `rooms` (`room_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `events`
+--
+
+LOCK TABLES `events` WRITE;
+/*!40000 ALTER TABLE `events` DISABLE KEYS */;
+INSERT INTO `events` VALUES (10,'EA11.101',3,'English','2023-06-07','10:30:00','13:15:00'),(11,'AP154.010',3,'Databases','2023-05-28','11:15:00','13:00:00'),(12,'AP154.010',3,'Databases','2023-05-27','11:15:00','13:00:00'),(14,'AP154.010',3,'Maths','2023-06-05','13:30:00','13:45:00'),(16,'EA11.101',3,'Algorithms','2023-06-05','11:15:00','14:00:00');
+/*!40000 ALTER TABLE `events` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rooms`
+--
+
+DROP TABLE IF EXISTS `rooms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rooms` (
+  `room_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`room_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rooms`
+--
+
+LOCK TABLES `rooms` WRITE;
+/*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
+INSERT INTO `rooms` VALUES ('AP154.010'),('EA11.101');
+/*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
-DROP TABLE IF EXISTS `users`;
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `user_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -43,13 +139,26 @@ CREATE TABLE `users` (
   `password` varchar(255) DEFAULT NULL,
   `role` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (2,'Gamal','gamal@gmail.com','gamal123','Student'),(3,'Benedikt','bene@benemail.com','bene123','Admin'),(4,'Petra','petramail@gmail.com','petra123','Assistant'),(5,'Anna','anna@annamail.com','anna123','Student'),(6,'Richard','richard@gmail.com','richard123','Admin');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `wishes`
 --
-DROP TABLE IF EXISTS `wishes`;
 
+DROP TABLE IF EXISTS `wishes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `wishes` (
   `wish_id` int NOT NULL AUTO_INCREMENT,
   `assistant_name` varchar(45) DEFAULT NULL,
@@ -59,4 +168,26 @@ CREATE TABLE `wishes` (
   `event_name` varchar(45) DEFAULT NULL,
   `room` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`wish_id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wishes`
+--
+
+LOCK TABLES `wishes` WRITE;
+/*!40000 ALTER TABLE `wishes` DISABLE KEYS */;
+INSERT INTO `wishes` VALUES (11,'Petra','2023-06-08','11:15:00','12:00:00','Algorithms','EA11.101');
+/*!40000 ALTER TABLE `wishes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-27 17:49:17
