a7563984c03415dacdc04a2d428fa04f1ac24d3c
Nakupenda <116508057+Nakupenda25@users.noreply.github.com>
2023-05-29 18:38:30 +0200

    Edited pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,7 @@
 open module com.example.javafxscheduler {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.hibernate.orm.core;
     requires java.sql;
-    requires jakarta.persistence;
     requires java.naming;
 
     exports com.example.javafxscheduler;
